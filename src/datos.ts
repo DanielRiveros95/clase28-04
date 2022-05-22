92cc3e3966008155c8e738d5c3769a85a900515c
DanielRiveros95 <104107281+DanielRiveros95@users.noreply.github.com>
2022-05-22 13:47:15 -0300

    Initial commit

---
+++ src/datos.ts
@@ -0,0 +1,16 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato1 = document.getElementById("dato1");
+let dato2 = document.getElementById("dato2");
+rotulo1. innerHTML ="ingrese la base"
+rotulo2. document.getElementById("rotulo");
+
+let base: number = Number(dato1.value);
+let altura: number = Number(dato2.value);
+
+let area: number = base * altura;
+
+
+    console.log("area");
+
+  });
+  
